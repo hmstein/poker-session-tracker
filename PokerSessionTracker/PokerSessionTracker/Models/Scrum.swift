948891fd1f8674ae21135a4dc563ce124958ddba
Harrison Stein <harrison.m.stein@gmail.com>
2025-03-11 09:50:55 -0400

    tutorial model

--- PokerSessionTracker/PokerSessionTracker/Models/Scrum.swift
+++ PokerSessionTracker/PokerSessionTracker/Models/Scrum.swift
@@ -11,3 +11,21 @@ struct Scrum {
     var lengthInMinutes: Int
     var theme: Theme
 }
+
+extension Scrum {
+    static let sampleData: [Scrum] =
+    [
+        Scrum(title: "Design",
+                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                   lengthInMinutes: 10,
+                   theme: .yellow),
+        Scrum(title: "App Dev",
+                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+                   lengthInMinutes: 5,
+                   theme: .orange),
+        Scrum(title: "Web Dev",
+                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+                   lengthInMinutes: 5,
+                   theme: .poppy)
+    ]
+}
